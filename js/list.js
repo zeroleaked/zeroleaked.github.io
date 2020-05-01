449ad4e58a762bd94ce2541801c44bdf57472392
zir <fakegags@gmail.com>
2020-05-02 06:11:31 +0700

    footer

---
+++ js/list.js
@@ -0,0 +1,13 @@
+$(function(){
+
+  $(".box").hide();
+
+  $(".label").click(function(){
+    $(this).siblings.show()
+  });
+
+  $(".label").click(function(){
+    $(this).siblings.show()
+  });
+  
+});
