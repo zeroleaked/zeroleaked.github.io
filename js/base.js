339f4a04ba82a8aae132e741ed67d078ab31a730
zir <fakegags@gmail.com>
2020-05-20 01:41:22 +0700

    Revert "hidden moved to css"
    This reverts commit c2b1002a1ee37e2005f5cee06328a1ed83a04c81.

--- js/base.js
+++ js/base.js
@@ -3,6 +3,9 @@ $(function(){
   $('#id_username').attr("placeholder", "username");
   $('#id_password').attr("placeholder", "password");
 
+  $('nav>form').hide();
+  $('nav label').hide();
+
   $('.login-button').click(() => {
     $('nav #login-form').toggle();
   })
