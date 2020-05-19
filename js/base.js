1d6c1e566c65f7ee24fe1e922780d30c3333b9af
zeroleaked <46643836+zeroleaked@users.noreply.github.com>
2020-05-20 03:44:05 +0700

    Update base.js

--- js/base.js
+++ js/base.js
@@ -3,6 +3,8 @@ $(function(){
   $('#id_username').attr("placeholder", "username");
   $('#id_password').attr("placeholder", "password");
 
+  $('#login-form label').hide();
+  
   $('.login-button').click(() => {
     $('nav #login-form').toggle();
   })
