faeb2bc4d6a509d896123e73e30e80f42c4626af
zir <fakegags@gmail.com>
2020-05-20 01:43:44 +0700

    corrected

--- js/base.js
+++ js/base.js
@@ -3,9 +3,6 @@ $(function(){
   $('#id_username').attr("placeholder", "username");
   $('#id_password').attr("placeholder", "password");
 
-  $('nav>form').hide();
-  $('nav label').hide();
-
   $('.login-button').click(() => {
     $('nav #login-form').toggle();
   })
