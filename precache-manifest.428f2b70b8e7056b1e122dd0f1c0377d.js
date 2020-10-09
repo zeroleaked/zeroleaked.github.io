cb09c466f8a2059b1abac4eb68161e3e7028cdca
zir <fakegags@gmail.com>
2020-10-10 02:08:35 +0700

    Updates

---
+++ precache-manifest.428f2b70b8e7056b1e122dd0f1c0377d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc47d8397fee651c38ecd9f82efb47cc",
+    "url": "/peslit/index.html"
+  },
+  {
+    "revision": "9f6db82f0661fa09e899",
+    "url": "/peslit/static/css/main.e0447824.chunk.css"
+  },
+  {
+    "revision": "28468aa5ed42e38934f7",
+    "url": "/peslit/static/js/2.a413f639.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/peslit/static/js/2.a413f639.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f6db82f0661fa09e899",
+    "url": "/peslit/static/js/main.36094278.chunk.js"
+  },
+  {
+    "revision": "87570138c2db905fc8a5",
+    "url": "/peslit/static/js/runtime-main.ae458c8c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/peslit/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "429f74b26fbd934251a7a0e1f7a754f5",
+    "url": "/peslit/static/media/pattern.429f74b2.png"
+  }
+]);
